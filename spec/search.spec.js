2406f3ddc55ab8d7d344ef60fc67eb1a95081581
Girma Nigusse <xgirma@gmail.com>
2020-03-24 17:45:07 -0400

    updates

--- spec/search.spec.js
+++ spec/search.spec.js
@@ -7,15 +7,9 @@ module.exports = {
     browser.end();
   },
 
-<<<<<<< HEAD
-  "should be on Sandbox": browser => {
-    browser.assert.title("Sandbox");
-    browser.getText("css selector", "h1", text => {
-=======
   "should be on Sandbox": (browser) => {
     browser.assert.title("Sandbox");
     browser.getText("css selector", "h1", (text) => {
->>>>>>> 9d36f393bd5275837eda6cd5fb31d872dd8767d0
       browser.assert.strictEqual(text.value, "Sandbox");
     });
   },
