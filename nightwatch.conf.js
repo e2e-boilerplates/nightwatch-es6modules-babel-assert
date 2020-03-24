5326946d0905535d109a5bb3678e1c2144a45e38
Girma Nigusse <xgirma@gmail.com>
2020-03-24 15:37:06 -0400

    updates

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,11 +1,7 @@
 /* eslint-disable-next-line global-require,import/no-extraneous-dependencies */
 require("@babel/register");
 
-<<<<<<< HEAD
-module.exports = (settings => {
-=======
 module.exports = ((settings) => {
->>>>>>> 9d36f393bd5275837eda6cd5fb31d872dd8767d0
   // eslint-disable-next-line no-param-reassign
   settings.test_workers = false;
   return settings;
